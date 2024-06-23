b62dd36d4d8efcb8de4ef4716c552c16eec6f672
Nurfadilah-Rahman-105841110421-Lab 1 <105841110421@student.unismuh.ac.id>
2024-06-24 01:25:48 +0800

    Text Input

--- App.js
+++ App.js
@@ -1,81 +1,35 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Text, View, Image, TextInput } from 'react-native';
 import React from 'react';
-import ButtonComponent from './component/button/button';
 
 const App = () => {
+  const Inputan = ({ nama, color })=>{
+    return(
+      <TextInput 
+      placeholder={`Masukkan ${nama}`}
+      style={{
+        borderWidth:1,
+        borderColor: 'black',
+        borderRadius: 10,
+        width: 300,
+        height: 50,
+        marginVertical: 10,
+        padding: 10,
+        backgroundColor: 'beige',
+        color: color,
+      }}/>
+    )
+  }
   return (
-    <View style={{
+    <View style = {{
       flex: 1,
-      backgroundColor: '#000',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
     }}>
-      <Image
-        source={require('./assets/lazadut.jpg')}
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          top: 15,
-          opacity: 0.4,
-          zIndex: -1,
-        }}
-        resizeMode="cover"
-      />
-      <ScrollView contentContainerStyle={{
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-      }}>
-        <Image
-          source={require('./assets/lazadut.png')}
-          style={{
-            width: 150,
-            height: 190,
-            marginTop: 50,
-            opacity: 1,
-            zIndex: 1,
-          }}
-          resizeMode="cover"
-        />
-        <Text style={{
-          fontSize: 20,
-          fontFamily: 'sans-serif', 
-          fontWeight: 'bold',
-          color: 'white',
-          position: 'absolute',
-          top: 0,
-          marginTop: 320,
-        }}>Selamat datang di Lazadut</Text> 
-        
-        <Text style={{
-          color: 'white',
-          fontSize: 18,
-          textAlign: 'center',
-          marginTop: 20,
-        }}>Lazadut adalah tempat terbaik untuk menemukan produk-produk terbaru dengan harga yang terjangkau</Text>
-       
-        <Text style={{
-          color: 'white',
-          fontSize: 18,
-          textAlign: 'center',
-          marginTop: 20,
-        }}>Jelajahi berbagai pilihan produk dari Lazadut dan nikmati pengalaman berbelanja online yang nyaman</Text>
-       
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 40,
-          marginBottom: 50,
-        }}>
-          <ButtonComponent title="Sign Up" color="brown"/>
-          <ButtonComponent title="Sign In" color="blue"/>
-        </View>
-      </ScrollView>
+      <Inputan nama = "email" color = "white"/>
+      <Inputan nama = "username" color = "white"/>
+      <Inputan nama = "password" color = "black"/>
     </View>
-  );
+  )  
 }
 
-export default App;
+export default App
---
+++ component/pages/login.js
@@ -0,0 +1,15 @@
+import { Text, View, Image, ScroolView } from 'react-native';
+import React from 'react';
+
+const login = () => {{
+    return (
+        <View style = {{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Text style = {{ color: 'white'}}>Login</Text>
+        </View>
+    )
+        
+}}
