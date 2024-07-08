f63e4681578d1ea83f483aa1a7dcc73e9eec99fe
Nurfadilah-Rahman-105841110421-Lab 1 <105841110421@student.unismuh.ac.id>
2024-07-08 23:27:16 +0800

    Use fonts

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
-import ForgotPassword from './component/pages/forgotPassword'; 
+import { useFonts } from 'expo-font';
+import { Text, View } from 'react-native';
 
-const App = () => {
+export default function App () {
+  const [dapatFont] = useFonts ({
+    'MetroBlack': require('./assets/font/Metropolis-Black.otf'),
+    'MetroBold' : require('./assets/font/Metropolis-Bold.otf'),
+    'MetroLight': require('./assets/font/Metropolis-Light.otf'),
+    'MetroMedium': require('./assets/font/Metropolis-Medium.otf'),
+    'MetroSemiBold': require('./assets/font/Metropolis-SemiBold.otf'),
+  });
+
+  if (!dapatFont) {
+    return <Text>Font tidak ditemukan silahkan coba lagi ...</Text>
+  }
   return (
-    <View style={{ flex: 1, backgroundColor: '#EEEEEE' }}>
-      <ForgotPassword/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{ fontFamily: 'MetroBlack'}}>Font Metro Black</Text>
+      <Text style={{ fontFamily: 'MetroBold'}}>Font Metro Bold</Text>
+      <Text style={{ fontFamily: 'MetroLight'}}>Font Metro Light</Text>
+      <Text style={{ fontFamily: 'MetroMedium'}}>Font Metro Medium</Text>
+      <Text style={{ fontFamily: 'MetroSemiBold'}}>Font Metro SemiBold</Text>
     </View>
-  );
-};
-
-export default App;
+  )
+}
